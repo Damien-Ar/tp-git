9e3ea2d831c9535bf7ad2356976734b45ce6ab78
Damien-Ar <damien.aranda42@gmail.com>
2024-03-23 15:51:20 +0100

    ajout écriture tree

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from pathlib import Path
 import zlib
 import hashlib
 
-GIT_DIR = ".git"
+GIT_DIR = ".notgit"
+GIT_OBJECTS_DIR = "objects"
+GIT_REFS_DIR = "refs"
+GIT_DATA_FOLDERS = [
+  GIT_OBJECTS_DIR,
+  GIT_REFS_DIR
+]
 
 def repo_root(path: str = None) -> Path:
   # Get current directory if unspecified
@@ -34,9 +40,10 @@ def is_sha1(s: str) -> bool:
 def object_path(sha1: str) -> Path:
   assert is_sha1(sha1), "Invalid sha1"
 
-  return repo_notgit_dir() / "objects" / sha1[:2] / sha1[2:]
+  return repo_notgit_dir() / GIT_OBJECTS_DIR / sha1[:2] / sha1[2:]
 
 def object_content(sha1: str) -> bytes:
+  print(sha1)
   try:
     with object_path(sha1).open(mode="rb") as f:
       return zlib.decompress(f.read())
@@ -66,9 +73,12 @@ def resolve_ref(ref : str) -> str:
 
   return resolve_ref(next_name)
 
-def hash_object(path: str, write:bool = False) -> str:
-  user_data = (repo_root() / path).read_bytes()
-  raw_content = f"blob {len(user_data)}\x00".encode() + user_data
+def hash_object(path: Path, object_type: str = "blob", write: bool = False) -> str:
+  user_data = path.read_bytes()
+  return hash_content(user_data, object_type=object_type, write=write)
+
+def hash_content(value: bytes, object_type: str = "tree", write: bool = False) -> str:
+  raw_content = f"{object_type} {len(value)}\x00".encode() + value
   sha1 = hashlib.sha1(raw_content).hexdigest()
 
   if write:
@@ -105,17 +115,34 @@ def print_history(_from: str) -> None:
     except KeyError:
       break
 
-def parse_tree_entry(raw_bytes: str) -> tuple:
+def parse_tree_entry(raw_bytes: bytes) -> tuple:
+  ### Entry : "mode raw_name\x00sha1"
   info, bin_sha1 = raw_bytes.split(b"\x00", maxsplit= 1)
   mode, raw_name = info.split(b" ")
   sha1 = bin_sha1.hex()
   type, _ = get_object(sha1)
   name = raw_name.decode("utf-8")
-  return mode, type, sha1, name
+  return mode.decode(), type.decode(), sha1, name
+
+def encode_tree_entry(file: Path) -> bytes:
+  # folder test : fe34e09301aab87fcbe2c323a9fd6abb9d709c0d
+  sha1 = hash_object(file)
+  mode = "100644".encode()
+  raw_name = file.name.encode()
+  info = mode + b" " + raw_name
+  return info + b"\x00" + bytes.fromhex(sha1)
+
+def encode_tree_entry_tree(path: Path, value: bytes) -> bytes:
+  sha1 = hash_content(value)
+  mode = "040000".encode()
+  raw_name = path.name.encode()
+  info = mode + b" " + raw_name
+  return info + b"\x00" + bytes.fromhex(sha1)
 
 def parse_tree(sha1 : str) -> list:
   type, data = get_object(sha1)
   assert type == b"tree"
+  print(data)
   entries = []
   start = 0
   while True:
@@ -129,6 +156,23 @@ def parse_tree(sha1 : str) -> list:
     start = end
 
   return entries
+
+def encode_tree(path: Path) -> bytes:
+  entries = []
+  for child in path.iterdir():
+    print(child.name)
+    if(child.is_file()):
+      entries.append(encode_tree_entry(child))
+    else:
+      entries.append(encode_tree_entry_tree(child, encode_tree(child)))
+    print(entries[-1])
+  return b"\x00".join(entries)
+
+def write_tree(path: Path) -> None:
+  tree_content = encode_tree(path)
+  print(tree_content)
+  print(hash_content(tree_content))
+  hash_content(tree_content, write=True)
 
 def restore_file(blob_sha1 : str, path: Path):
   type, data = get_object(blob_sha1)
@@ -161,11 +205,23 @@ def switch(ref: str) -> None:
   new_head = sha1 if ref == sha1 else f"ref: {ref}"
   (repo_notgit_dir() / "HEAD").write_text(new_head)  
 
-print(repo_notgit_dir("./test"))
-print(get_object("5fc3a27876f4d94b666562d74b62627d33879aa1"))
-print(resolve_ref("HEAD"))
-print(resolve_ref("5fc3a27876f4d94b666562d74b62627d33879aa1"))
-print(hash_object("./test/test.txt"))
-print(parse_commit(resolve_ref("HEAD")))
-print_history("HEAD")
-print(parse_tree(parse_commit(resolve_ref("HEAD"))["tree"]))
+def initialise_repo(path: str=None) -> None : 
+  base_path = Path.cwd() if path is None else Path(path).resolve()
+  repo_path = base_path / GIT_DIR
+  try:
+    repo_path.mkdir()
+    for dir in GIT_DATA_FOLDERS:
+      (repo_path / dir).mkdir()
+  except:
+    print("There is already a repository")
+
+
+
+# print(repo_notgit_dir("./test"))
+# print(get_object("5fc3a27876f4d94b666562d74b62627d33879aa1"))
+# print(resolve_ref("HEAD"))
+# print(resolve_ref("5fc3a27876f4d94b666562d74b62627d33879aa1"))
+# print(hash_object(Path("./test/test.txt")))
+# print(parse_commit(resolve_ref("HEAD")))
+# print_history("HEAD")
+# print(parse_tree(parse_commit(resolve_ref("HEAD"))["tree"]))
